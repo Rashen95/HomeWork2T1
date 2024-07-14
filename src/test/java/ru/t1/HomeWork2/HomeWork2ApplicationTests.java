dfedb0258fe50dbe901467707864c97467101bf6
Artem Privalov <rashen95@mail.ru>
2024-07-14 20:39:52 +0300

    initial commit

---
+++ src/test/java/ru/t1/HomeWork2/HomeWork2ApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.t1.HomeWork2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork2ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
